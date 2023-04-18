7a25da470324cc36f26569c03c5c467e1d82648a
AhmedGamal <ahmedgamalzenhom@gmail.com>
2023-04-18 21:34:54 +0200

    start project

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
+import { NextUIProvider } from '@nextui-org/react'
+
+export default function App({ Component, pageProps:{session,...pageProps} }) {
+  return (
+    <NextUIProvider>
+  <SessionProvider session={session}>
+  <Component {...pageProps} />
+  </SessionProvider>
+  </NextUIProvider>
+  )
+}
