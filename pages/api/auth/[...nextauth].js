cf77ae819a86bd6a05315f3e9e1a1a27cd6cd1ac
AhmedGamal <ahmedgamalzenhom@gmail.com>
2023-05-02 00:38:59 +0300

    add provider facebook,gitHub,google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,43 +2,47 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
-import  CredentialsProvider from 'next-auth/providers/credentials';
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
-
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
-        email: { label: "email", type: "text", placeholder: "Enter yor email " },
-        password: { label: "Password", type: "password" }
+        email: {
+          label: "email",
+          type: "text",
+          placeholder: "Enter yor email ",
+        },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-         const user={id:1,email:"AhmedGamal@gmail.com", password: "Ahmed" }
+        const res = await fetch("http://jsonplaceholder.typicode.com/users");
+        const user = await res.json();
 
-         if (credentials.email==user.email && credentials.password==user.password) 
-             return user    
-         else
-               null 
-        } 
-      }),
-  
+        for (let i = 0; i < user.length; i++) {
+          if (credentials.email == user[i].email) {
+            return user[i];
+          } else {
+            ("is null");
+          }
+        }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-      FacebookProvider({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-      }),
-     
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
   ],
-  secret:process.env.JWT_SECRET
+  secret: process.env.JWT_SECRET,
+};
 
-}
-
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
